ea15169e73fb39606a461fe5763f807c18abf0ec
Ashutosh V M <166061983+ashutoshvm-off@users.noreply.github.com>
2024-10-25 21:54:35 +0530

    updated files

--- cert.py
+++ cert.py
@@ -1,14 +1,16 @@
+
 import pandas as pd
 from PIL import Image, ImageFont, ImageDraw
+import os
 
 
-FONT_FILE = ImageFont.truetype('AlbertSans-Bold.ttf', 12)#chage the font and size here
+FONT_FILE = ImageFont.truetype('ShipporiMincho-Medium.ttf', 36)#chage the font and size here
 FONT_COLOR = "#000000"
-WIDTH, HEIGHT  = 0,0 # define certificate actual length/2 and bridth/2 or point where you wanna start writing
+WIDTH, HEIGHT  = 528,448 # define certificate actual length/2 and bridth/2 or point where you wanna start writing
 
 def make_cert(name):
-  
-    image_source = Image.open('enter certificate source.extension')
+    os.makedirs('out', exist_ok=True)
+    image_source = Image.open('2.png')
     draw = ImageDraw.Draw(image_source)
     bbox = draw.textbbox((0, 0), name, font=FONT_FILE)
     #print(bbox)
@@ -17,11 +19,11 @@ def make_cert(name):
     name_height = bbox[3] - bbox[1]
 
     draw.text((WIDTH-name_width/2, HEIGHT-name_height), name, fill=FONT_COLOR, font=FONT_FILE)#adjust the width / value according to the postion keep it in /2 to make it center
-    
-    image_source.save("./out/" + name+".pdf")
+    path = os.path.join('out', name+'.png')
+    image_source.save(path)
     print('printing certificate: '+name)
 
-data=pd.read_excel('name.xlsx')
+data=pd.read_excel('data.xlsx')
 names = list(data.name)
 for x in names:
     make_cert(x)
--- mail.py
+++ mail.py
@@ -8,7 +8,7 @@ import os
 # Configuration
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
-EMAIL_ADDRESS = ''#add your mail id here
+EMAIL_ADDRESS = 'xxxxxxxx@gmail.com'#add your mail id here
 EMAIL_PASSWORD = ''#add your app password here
 EXCEL_FILE = 'data.xlsx'
 DOCUMENTS_FOLDER = 'out'  # Folder where your documents are stored
@@ -18,8 +18,8 @@ df = pd.read_excel(EXCEL_FILE)
 
 # Function to send email
 def send_email(to_address, name):
-    subject = f"Enter your subject here"
-    body = f"Dear {name},\n\nEnter your content here\n\nBest regards,\ncontent ,\n content"
+    subject = f"xxxxx" #change the content
+    body = f"Dear {name},\n\n xxxxxx\n\nBest regards,\nxxx\n xxxx"
 
     # Create the email
     msg = MIMEMultipart()
@@ -28,23 +28,37 @@ def send_email(to_address, name):
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
 
-    # Attach the document if it exists
+    #Attach the document if it exists
     document_path = os.path.join(DOCUMENTS_FOLDER, f"{name}.pdf")  # Change extension if needed
     if os.path.exists(document_path):
-        with open(document_path, 'rb') as file:
-            attach = MIMEApplication(file.read(), _subtype='pdf')
-            attach.add_header('Content-Disposition', 'attachment', filename=os.path.basename(document_path))
-            msg.attach(attach)
-
+       with open(document_path, 'rb') as file:
+           attach = MIMEApplication(file.read(), _subtype='pdf')
+           attach.add_header('Content-Disposition', 'attachment', filename=os.path.basename(document_path))
+           msg.attach(attach)
+    return msg
     # Send the email
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-        server.starttls()
-        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        server.send_message(msg)
-        print(f"Email sent to {name} at {to_address}")
+    # with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+    #     # server.starttls()
+    #     # server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    #     server.send_message(msg)
+    #     
 
 # Loop through each row in the DataFrame
-for index, row in df.iterrows():
-    name = row['name']
-    email_address = row['Email id']
-    send_email(email_address, name)
+# for index, row in df.iterrows():
+#     name = row['name']
+#     email_address = row['Email Address']
+#     send_email(email_address, name)
+
+
+if __name__== '__main__':
+     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+
+        for index, row in df.iterrows():
+            name = row['name']
+            email_address = row['Email Address']
+            msg = send_email(email_address, name)
+            server.send_message(msg)
+            print(f"Email sent to {name} at {email_address}")
